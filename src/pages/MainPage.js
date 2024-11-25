6cea420fdcec0d42367b3261391b8a0cd03ae214
Rhuan Lopes <rhuanlopesdev@gmail.com>
2024-11-25 00:40:10 -0300

    Adicionando chamada post para criar novos eventos pelo formulário de criação de eventos

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,7 +2,7 @@ import { Button, Card, Col, Form, OverlayTrigger, Row, Tooltip } from "react-boo
 import { useEffect, useState } from "react";
 import '../styles/MainPage.css'
 import { useDispatch, useSelector } from "react-redux";
-import { fetchEventList, selectEventList, selectEventsStatus } from "../store/slices/EventSlice";
+import { addNewEvent, fetchEventList, selectEventList, selectEventsStatus } from "../store/slices/EventSlice";
 
 const MainPage = () => {
     const eventList = useSelector(state => selectEventList(state));
@@ -21,12 +21,27 @@ const MainPage = () => {
         </Tooltip>
     );
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //  para selecionar apenas os elementos de dados do formulario
+        let inputArrays = Array.from(e.currentTarget.elements).filter((el) => el.type !== "button" && el.type !== "submit");
+
+        // para formatar os dados no formato de um evento utilizando os ids dos campos
+        const newEventData = inputArrays.reduce((acc, val) => {
+            var obj = { ...acc };
+            obj[val.id.replace('form-event-', '')] = val.value;
+            return obj;
+        }, {})
+
+        dispatch(addNewEvent(newEventData));
+    }
+
     return (<div fluid="sm" className="mx-auto  main-container pt-5 mb-5  gx-4 gy-5 row justify-content-center" style={{ width: '90%' }} >
         <h1 className="col-12"> Eventos </h1>
         {
             eventList.map((val, index) =>
                 <div className="main-card-container col-sm-6 col-lg-4" style={{ minWidth: '350px' }} key={'event-card-' + index}>
-                    <Card className="main-card " >
+                    <Card className="main-card h-100" >
                         <Card.Header className="p-0">
                             <Card.Img src="https://static.todamateria.com.br/upload/ab/ob/aboboras2-cke.jpg" />
                         </Card.Header>
@@ -62,8 +77,8 @@ const MainPage = () => {
         }
 
         {showCreationForm ? (
-            <Form className="border  rounded-1 position-fixed w-75 h-75 bg-body py-4 px-3" style={{}}  >
-                <Row className="d-flex px-2 justify-content-between">
+            <Form className="border rounded-1 position-fixed w-75 h-75 bg-body py-4 px-3 overflow-y-scroll" style={{}} onSubmit={handleSubmit}  >
+                <Row className="d-flex my-3 px-2 justify-content-between">
                     <div style={{ width: 'auto' }}>Criar Evento</div>
                     <Button className="rounded-circle bg-body border-0" style={{ width: 'auto' }} onClick={() => setShowCreationForm(false)}>
                         <i className="bi bi-x-lg" width="32" height="32"></i>
@@ -71,17 +86,59 @@ const MainPage = () => {
                     </Button>
                 </Row>
 
-                <Row>
-                    <Form.Group as={Col} controlId="formGridEventName">
+                <Row className="my-3">
+                    <Form.Group as={Col} controlId="form-event-title">
                         <Form.Label>Nome do Evento</Form.Label>
-                        <Form.Control placeholder="Digite o nome" />
+                        <Form.Control required placeholder="Digite o nome" />
                     </Form.Group>
-                    <Form.Group as={Col} controlId="formGridEventLocation">
+                    <Form.Group as={Col} controlId="form-event-location">
                         <Form.Label>Local do Evento</Form.Label>
-                        <Form.Control placeholder="Selecione o Local" />
+                        <Form.Control required placeholder="Selecione o Local" />
                     </Form.Group>
                 </Row>
 
+                <Row className="my-3">
+                    <Form.Group as={Col} controlId="form-event-main_image">
+                        <Form.Label>Insira a imagem do Evento</Form.Label>
+                        <Form.Control required type="file" />
+                    </Form.Group>
+                </Row>
+                <Row className="my-3">
+                    <Form.Group as={Col} controlId="form-event-date">
+                        <Form.Label>Selecione a data do evento</Form.Label>
+                        <Form.Control required type="date" />
+                    </Form.Group>
+                    <div className="d-flex col">
+                        <Form.Group as={Col} controlId="form-event-start_time">
+                            <Form.Label>Início:</Form.Label>
+                            <Form.Control className="me-1" required type="time" placeholder="Início" />
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="form-event-end_time">
+                            <Form.Label>Final:</Form.Label>
+                            <Form.Control className="ms-1" placeholder="Final" required type="time" />
+                        </Form.Group>
+                    </div>
+                </Row>
+
+                <Row className="my-3">
+                    <Form.Group as={Col} controlId="form-event-capacity">
+                        <Form.Label>Selecione a capacidade do evento:</Form.Label>
+                        <Form.Control required type="number"></Form.Control>
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="form-event-price">
+                        <Form.Label>Selecione o preço do evento:</Form.Label>
+                        <Form.Control required type="number"></Form.Control>
+                    </Form.Group>
+                </Row>
+
+                <Row>
+                    <Form.Group controlId="form-event-description">
+                        <Form.Label>Descrição do evento:</Form.Label>
+                        <Form.Control required as="textarea" rows="3" />
+                    </Form.Group>
+                </Row>
+
+                <Button type="submit" className="mt-4">Criar evento</Button>
             </Form>
         ) : (
             <OverlayTrigger
--- src/store/slices/EventSlice.js
+++ src/store/slices/EventSlice.js
@@ -2,9 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchEventList = createAsyncThunk('event/fetchEventList', async () => {
     const response = await (await fetch('http://localhost:3004/events')).json();
-    console.log(response)
     return response;
-
 }
     , {
         condition(arg, thunkApi) {
@@ -15,11 +13,20 @@ export const fetchEventList = createAsyncThunk('event/fetchEventList', async () 
         }
     });
 
+export const addNewEvent = createAsyncThunk('event/addNewEvent', async (newEvent) => {
+    const response = await fetch('http://localhost:3004/events', {
+        method: 'POST',
+        body: JSON.stringify(newEvent),
+    });
+    return response;
+});
+
 export const eventSlice = createSlice({
     name: 'Event',
     initialState: {
         eventList: [],
         status: 'idle',
+        statusCreation: 'idle',
         error: null
     },
 
@@ -30,20 +37,33 @@ export const eventSlice = createSlice({
         }
     },
 
-    extraReducers: builder => {
-        builder.addCase(fetchEventList.pending, (state, action) => {
-            state.status = 'pending';
-        }).addCase(fetchEventList.fulfilled, (state, action) => {
-            state.status = 'completed';
-            console.log(action.payload);
-            state.eventList = action.payload;
-        }).addCase(fetchEventList.rejected, (state, action) => {
-            state.status = 'failed';
-            state.error = action.error.message ?? 'Erro desconhecido';
+    extraReducers:
+        // {
+        //     [fetchEventList.pending]: (state, action) => { console.log('state pending: ' + state) },
+        //     [fetchEventList.fulfilled]: (state, action) => { console.log('state fulfilled: ' + state) },
+        //     [fetchEventList.rejected]: (state, action) => { console.log('state rejected: ' + state) }
+        // }
+        builder => {
+            builder.addCase(fetchEventList.pending, (state, action) => {
+                state.status = 'pending';
+            }).addCase(fetchEventList.fulfilled, (state, action) => {
+                state.status = 'completed';
+                state.eventList = action.payload;
+            }).addCase(fetchEventList.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message ?? 'Erro ao buscar listagem de eventos';
 
-        });
+            }).addCase(addNewEvent.pending, (state, action) => {
+                state.statusCreation = 'pending';
+            }).addCase(addNewEvent.fulfilled, (state, action) => {
+                state.status = 'completed';
+                console.log(action.payload)
+            }).addCase(addNewEvent.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message ?? 'Erro ao criar evento'
+            });
 
-    }
+        }
 })
 
 
