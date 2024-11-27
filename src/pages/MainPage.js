f30640479c49f17c28ab06966fed17f86d857ee4
Rhuan Lopes <rhuanlopesdev@gmail.com>
2024-11-27 00:53:56 -0300

    Resolvendo conflitos

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,13 +2,6 @@ import { Button, Card, Col, Form, OverlayTrigger, Row, Tooltip } from "react-boo
 import { useState } from "react";
 import { defaultEventList } from "../models/Event";
 import '../styles/MainPage.css'
-<<<<<<< Updated upstream
-=======
-import { useDispatch, useSelector } from "react-redux";
-import { addNewEvent, fetchEventList, selectEventList, selectEventsStatus } from "../store/slices/EventSlice";
-import Header from '../components/header/header';
->>>>>>> Stashed changes
-
 const MainPage = () => {
     const [eventList] = useState(defaultEventList);
     const [showCreationForm, setShowCreationForm] = useState(false);
@@ -19,13 +12,7 @@ const MainPage = () => {
     );
 
     return (<div fluid="sm" className="mx-auto  main-container pt-5 mb-5  gx-4 gy-5 row justify-content-center" style={{ width: '90%' }} >
-<<<<<<< Updated upstream
         <h1 className="col-12"> Eventos</h1>
-=======
-       
- 
-        <h1 className="col-12"> Eventos </h1>
->>>>>>> Stashed changes
         {
             eventList.map((val, index) =>
                 <div className="main-card-container col-sm-6 col-lg-4" style={{ minWidth: '350px' }} key={'event-card-' + index}>
@@ -64,41 +51,42 @@ const MainPage = () => {
                 </div>)
         }
 
-        {showCreationForm ? (
-            <Form className="border  rounded-1 position-fixed w-75 h-75 bg-body py-4 px-3" style={{}}  >
-                <Row className="d-flex px-2 justify-content-between">
-                    <div style={{ width: 'auto' }}>Criar Evento</div>
-                    <Button className="rounded-circle bg-body border-0" style={{ width: 'auto' }} onClick={() => setShowCreationForm(false)}>
-                        <i className="bi bi-x-lg" width="32" height="32"></i>
+        {
+            showCreationForm ? (
+                <Form className="border  rounded-1 position-fixed w-75 h-75 bg-body py-4 px-3" style={{}}  >
+                    <Row className="d-flex px-2 justify-content-between">
+                        <div style={{ width: 'auto' }}>Criar Evento</div>
+                        <Button className="rounded-circle bg-body border-0" style={{ width: 'auto' }} onClick={() => setShowCreationForm(false)}>
+                            <i className="bi bi-x-lg" width="32" height="32"></i>
+                            {showCreationForm}
+                        </Button>
+                    </Row>
+
+                    <Row>
+                        <Form.Group as={Col} controlId="formGridEventName">
+                            <Form.Label>Nome do Evento</Form.Label>
+                            <Form.Control placeholder="Digite o nome" />
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="formGridEventLocation">
+                            <Form.Label>Local do Evento</Form.Label>
+                            <Form.Control placeholder="Selecione o Local" />
+                        </Form.Group>
+                    </Row>
+
+                </Form>
+            ) : (
+                <OverlayTrigger
+                    placement="bottom"
+                    delay={{ show: 100, hide: 100 }}
+                    overlay={renderTooltip}
+                >
+                    <Button className="bg-primary position-fixed z-3 rounded-circle" onClick={() => setShowCreationForm(true)} style={{ width: '75px', height: '75px', right: '3%', bottom: '6.5rem' }}>
+                        <i className="bi bi-plus" width="32" height="32"></i>
                         {showCreationForm}
                     </Button>
-                </Row>
+                </OverlayTrigger>
 
-                <Row>
-                    <Form.Group as={Col} controlId="formGridEventName">
-                        <Form.Label>Nome do Evento</Form.Label>
-                        <Form.Control placeholder="Digite o nome" />
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="formGridEventLocation">
-                        <Form.Label>Local do Evento</Form.Label>
-                        <Form.Control placeholder="Selecione o Local" />
-                    </Form.Group>
-                </Row>
-
-            </Form>
-        ) : (
-            <OverlayTrigger
-                placement="bottom"
-                delay={{ show: 100, hide: 100 }}
-                overlay={renderTooltip}
-            >
-                <Button className="bg-primary position-fixed z-3 rounded-circle" onClick={() => setShowCreationForm(true)} style={{ width: '75px', height: '75px', right: '3%', bottom: '6.5rem' }}>
-                    <i className="bi bi-plus" width="32" height="32"></i>
-                    {showCreationForm}
-                </Button>
-            </OverlayTrigger>
-
-        )
+            )
         }
     </div >);
 }
