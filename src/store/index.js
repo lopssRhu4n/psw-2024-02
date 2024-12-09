b8119dd20dada1dd0aed24c45aca6d222f7ef110
Luiz Guilherme <luizsilvags07@gmail.com>
2024-12-09 08:56:28 -0300

    Atualizações 'index.js'

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,9 @@ import { configureStore } from '@reduxjs/toolkit'
 import eventReducer from './slices/EventSlice';
 import authReducer from './slices/AuthSlice';
 import inviteReducer from './slices/InviteSlice';
+import feedbackReducer from './slices/FeedbackSlice';
 
 export const store = configureStore({
-    reducer: { event: eventReducer, auth: authReducer, invite: inviteReducer }
+    reducer: { event: eventReducer, auth: authReducer, invite: inviteReducer, feedback: feedbackReducer }
 })
 
